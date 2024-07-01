50c84e9953da7388fb54dbe3909cd68cc975b9e6
Elizer Dolorosa <zykisriel0434@gmail.com>
2024-07-01 19:22:43 +0800

    init header for master layout

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,3 +1,9 @@
-<div class="text">
-    CPDO - PPA Fillup Form
+<div class="h-12 min-h-12 bg-green-600 flex-row flex w-full items-center">
+    <a href="{{url('/')}}" class="h-full">
+        <!-- <img src="/images/cityhall.jpg" class="h-full"> -->
+        <img src="{{ Vite::asset('/public/images/cityhall.jpg') }}" class="h-full">
+    </a>
+    <div class="text-white font-medium">
+        CPDO - PPA Fillup Form
+    </div>
 </div>
