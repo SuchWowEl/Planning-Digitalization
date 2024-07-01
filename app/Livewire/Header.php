ba84f8d3b0a1f158fd3432e64ac267b2f3c95636
Elizer Dolorosa <zykisriel0434@gmail.com>
2024-07-01 19:27:21 +0800

    ref: remove unneccesary import

--- app/Livewire/Header.php
+++ app/Livewire/Header.php
@@ -2,12 +2,10 @@
 
 namespace App\Livewire;
 
-use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class Header extends Component
 {
-
     public function render()
     {
         return view('livewire.header');
