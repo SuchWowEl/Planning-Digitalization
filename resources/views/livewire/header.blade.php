4d9aec0fdaf973a56e9d8918a5fb898dc94e0229
Elizer Dolorosa <zykisriel0434@gmail.com>
2024-07-01 20:55:34 +0800

    refactor: change header color to green, some spacing, along with responsive design for landing

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,9 +1,9 @@
-<div class="h-12 min-h-12 bg-green-600 flex-row flex w-full items-center">
-    <a href="{{url('/')}}" class="h-full">
+<div class="h-16 min-h-12 bg-[#145ab4] flex-row flex w-full items-center">
+    <a href="{{url('/')}}" class="h-full ml-5 mr-5 py-1">
         <!-- <img src="/images/cityhall.jpg" class="h-full"> -->
         <img src="{{ Vite::asset('/public/images/cityhall.png') }}" class="h-full">
     </a>
-    <div class="text-white font-medium">
+    <div class="text-white text-3xl font-medium">
         CPDO - PPA Fillup Form
     </div>
 </div>
--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -1,6 +1,6 @@
-<div class="h-[calc(100vh-3rem)] w-full bg-sky-500 text-white lg:px-32">
-    <div class="lg:h-full w-full flex flex-row flex-wrap">
-        <div class="lg:h-full h-36 lg:w-3/5 w-full text-white lg:text-5xl text-xl flex flex-col items-center justify-center rounded">
+<div class="h-[calc(100vh-4rem)] w-full bg-sky-500 text-white lg:px-32 sm:px-24">
+    <div class="md:h-full w-full flex flex-row flex-wrap">
+        <div class="md:h-full h-36 md:w-3/5 w-full text-white md:text-5xl text-3xl flex flex-col items-center justify-center rounded">
             <div class="mb-5">
                 Welcome!
             </div>
@@ -9,7 +9,7 @@
             </div>
         </div>
         <!-- min-h-[600px] -->
-        <div class="lg:h-full h-[600px] lg:w-2/5 w-full flex flex-col justify-evenly px-5">
+        <div class="md:h-full h-[600px] md:w-2/5 w-full flex flex-col justify-evenly px-5">
             <!-- h-36  -->
             @php
             $pair = [
@@ -20,8 +20,8 @@
             @endphp
 
             @foreach ($pair as $entry)
-            <a href="{{$entry[0]}}" class="lg:h-24 min-h-36 bg-white hover:bg-sky-400 lg:text-2xl text-lg text-sky-600 hover:text-white transition flex flex-col items-center justify-center
-            rounded hover:border-white hover:border-2">
+            <a href="{{$entry[0]}}" class="md:h-24 min-h-36 bg-white hover:bg-green-600 md:text-2xl text-3xl text-sky-600 hover:text-white transition flex flex-col items-center justify-center
+            rounded border-2 hover:border-white text-center">
                 {{$entry[1]}}
             </a>
             @endforeach
